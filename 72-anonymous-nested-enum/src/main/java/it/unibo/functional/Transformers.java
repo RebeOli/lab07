56b47a1790f793e5a93e682ab86f3b5f7cf9f2ed
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2025-11-06 14:38:48 +0100

    Ex. 72: Part 1 and 2 finished

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,11 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<? extends O>>() {
+            public Collection<O> call(I input){
+                return List.of(transformer.call(input));
+            }
+        });
     }
 
     /**
