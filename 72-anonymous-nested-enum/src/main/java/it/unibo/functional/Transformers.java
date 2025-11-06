6b91958b605d530e11489c9c8bda36e060876b76
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2025-11-06 15:58:07 +0100

    Part 2 has been corrected

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -74,7 +74,7 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
@@ -91,7 +91,15 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<? extends I>>() {
+            public Collection<I> call(I input){
+                Collection<I> result=new ArrayList<>();
+                if(test.call(input)){
+                    result.add(input);
+                }
+                return result;
+            }
+        });
     }
 
     /**
@@ -107,6 +115,10 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return select(base, new Function<I,Boolean>() {
+             public Boolean call(I input){
+                return !test.call(input);
+            }
+        });
     }
 }
